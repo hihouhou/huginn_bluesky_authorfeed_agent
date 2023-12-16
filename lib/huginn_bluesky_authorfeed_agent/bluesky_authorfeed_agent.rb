a158fe697b261334b66cd8d35d2b5a081212f07a
Nicolas Germain <ngermain@hihouhou.com>
2023-12-16 10:59:52 +0100

    limit option added

--- lib/huginn_bluesky_authorfeed_agent/bluesky_authorfeed_agent.rb
+++ lib/huginn_bluesky_authorfeed_agent/bluesky_authorfeed_agent.rb
@@ -7,16 +7,13 @@ module Agents
 
     description do
       <<-MD
-      The Bluesky Publish Agent publishes posts from the events it receives.
-
-      To be able to use this Agent you need to authenticate with Twitter with [twurl](https://github.com/twitter/twurl).
-
-      You must also specify a `message` parameter, you can use [Liquid](https://github.com/huginn/huginn/wiki/Formatting-Events-using-Liquid) to format the message.
-      Additional parameters can be passed via `parameters`.
+      The Bluesky Authorfeed Agent checks user's feed.
 
       `debug` is used for verbose mode.
 
       `handle` is mandatory for authentication.
+
+      `limit` is needed to limit the number of results.
 
       `user` is the wanted user you want to check.
 
@@ -31,6 +28,39 @@ module Agents
       Events look like this:
 
           {
+            "post": {
+              "uri": "at://XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/app.bsky.feed.post/XXXXXXXXXXXXX",
+              "cid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+              "author": {
+                "did": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "handle": "hihouhou.bsky.social",
+                "viewer": {
+                  "muted": false,
+                  "blockedBy": false,
+                  "followedBy": "at://XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/app.bsky.graph.follow/XXXXXXXXXXXXX"
+                },
+                "labels": [
+
+                ]
+              },
+              "record": {
+                "text": "test for authorfeed agent (huginn)",
+                "$type": "app.bsky.feed.post",
+                "langs": [
+                  "fr"
+                ],
+                "createdAt": "2023-12-15T20:21:50.150Z"
+              },
+              "replyCount": 1,
+              "repostCount": 0,
+              "likeCount": 0,
+              "indexedAt": "2023-12-15T20:21:50.150Z",
+              "viewer": {
+              },
+              "labels": [
+
+              ]
+            }
           }
     MD
 
@@ -39,6 +69,7 @@ module Agents
         'user' => '',
         'debug' => 'false',
         'handle' => '',
+        'limit' => '10',
         'app_password' => '',
         'expected_receive_period_in_days' => '2',
       }
@@ -48,6 +79,7 @@ module Agents
     form_configurable :debug, type: :boolean
     form_configurable :app_password, type: :string
     form_configurable :handle, type: :string
+    form_configurable :limit, type: :string
     form_configurable :expected_receive_period_in_days, type: :string
     def validate_options
       unless options['user'].present?
@@ -60,6 +92,10 @@ module Agents
 
       unless options['handle'].present?
         errors.add(:base, "handle is a required field")
+      end
+
+      unless options['limit'].present?
+        errors.add(:base, "limit is a required field")
       end
 
       if options.has_key?('debug') && boolify(options['debug']).nil?
@@ -129,7 +165,7 @@ module Agents
     def get_feed()
 
       did = generate_did(interpolated['handle'])
-      uri = URI.parse("https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed?actor=#{generate_did(interpolated['user'])}&limit=30")
+      uri = URI.parse("https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed?actor=#{generate_did(interpolated['user'])}&limit=#{interpolated['limit']}")
       request = Net::HTTP::Get.new(uri)
       request["Authorization"] = "Bearer #{generate_api_key(did)}"
     
